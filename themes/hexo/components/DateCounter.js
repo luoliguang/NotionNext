071cb593fa661fbe0ffff1fdb2ac2c11a9cd5789
giluo <giluo@vip.qq.com>
2023-11-29 23:42:59 +0800

    Update DateCounter.js
    新增全局变量

--- themes/hexo/components/DateCounter.js
+++ themes/hexo/components/DateCounter.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import CONFIG from 'themes/hexo/config'; // 导入CONFIG
+
+const DateCounter = () => {
+  // 从CONFIG中提取所需的值
+  const { SITE_START_DATE, HOLIDAY_NAME, HOLIDAY_DATE } = CONFIG;
+
+  // 组件的其余部分...
+  import React, { useState, useEffect } from 'react';
 
 const formatDateDifference = (startDate, endDate) => {
-  const diff = endDate - startDate;
+  let diff = endDate.getTime() - startDate.getTime();
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  diff -= days * (1000 * 60 * 60 * 24);
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  diff -= hours * (1000 * 60 * 60);
+  const minutes = Math.floor(diff / (1000 * 60));
+  diff -= minutes * (1000 * 60);
+  const seconds = Math.floor(diff / 1000);
 
   // 将小时、分钟和秒转换为双位数格式
   const formatDoubleDigit = (num) => num.toString().padStart(2, '0');
@@ -18,10 +29,9 @@ const formatDateDifference = (startDate, endDate) => {
   };
 };
 
-
 const DateCounter = ({ startDate, holidayName, holidayDate }) => {
   const [siteRuntime, setSiteRuntime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-  const [daysUntilHoliday, setDaysUntilHoliday] = useState(0);
+  const [holidayCountdown, setHolidayCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
     const start = new Date(startDate);
@@ -30,7 +40,7 @@ const DateCounter = ({ startDate, holidayName, holidayDate }) => {
     const updateCounter = () => {
       const now = new Date();
       setSiteRuntime(formatDateDifference(start, now));
-      setDaysUntilHoliday(formatDateDifference(now, holiday).days);
+      setHolidayCountdown(formatDateDifference(now, holiday));
     };
 
     updateCounter();
@@ -42,15 +52,19 @@ const DateCounter = ({ startDate, holidayName, holidayDate }) => {
   return (
     <div className="flex flex-col items-start justify-start p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md">
       <div className="text-lg font-semibold text-gray-700 dark:text-gray-300">
-        <h3 className="mb-2">网站运行时间</h3>
+        <h3 className="mb-2">网站已经运行</h3>
         <p className="text-xl">{siteRuntime.days}天 {siteRuntime.hours}小时 {siteRuntime.minutes}分钟 {siteRuntime.seconds}秒</p>
       </div>
       <div className="mt-4 text-lg font-semibold text-gray-700 dark:text-gray-300">
         <h3 className="mb-2">距离{holidayName}还有</h3>
-        <p className="text-xl">{daysUntilHoliday} 天</p>
+        <p className="text-xl">{holidayCountdown.days}天 {holidayCountdown.hours}小时 {holidayCountdown.minutes}分钟 {holidayCountdown.seconds}秒</p>
       </div>
     </div>
   );
+};
+
+export default DateCounter;
+
 };
 
 export default DateCounter;
