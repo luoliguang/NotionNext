e91112df5c3abae518f391908e94e440360df0df
giluo <giluo@vip.qq.com>
2024-05-10 00:43:15 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -36,7 +36,7 @@ const CONFIG = {
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
     
   // 添加新的配置项
-  SITE_START_DATE: '2023-11-24', // 网站启动日期
+  //SITE_START_DATE: '2023-11-24', // 网站启动日期
   /*
   HOLIDAY_NAME: '春节', // 节日名称
   HOLIDAY_DATE: '2024-02-10', // 节日日期
